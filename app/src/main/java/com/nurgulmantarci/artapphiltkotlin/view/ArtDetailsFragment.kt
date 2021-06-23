d92f63dc6cf95f2956c581c58c1d6d6fd99de011
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-23 16:07:23 +0300

    ui fragments created

---
+++ app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ArtDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.nurgulmantarci.artapphiltkotlin.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.nurgulmantarci.artapphiltkotlin.R
+
+class ArtDetailsFragment: Fragment(R.layout.fragment_art_details) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ImageApiFragment.kt
@@ -0,0 +1,13 @@
+package com.nurgulmantarci.artapphiltkotlin.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.nurgulmantarci.artapphiltkotlin.R
+
+class ImageApiFragment : Fragment(R.layout.fragment_image_api){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
