6d8a8122909b1e5065ecb431c7326f61d9561e2b
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-23 16:26:12 +0300

    navigation host added

--- app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ArtDetailsFragment.kt
@@ -3,11 +3,22 @@ package com.nurgulmantarci.artapphiltkotlin.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nurgulmantarci.artapphiltkotlin.R
+import com.nurgulmantarci.artapphiltkotlin.databinding.FragmentArtDetailsBinding
 
 class ArtDetailsFragment: Fragment(R.layout.fragment_art_details) {
+
+    private var fragmentBinding: FragmentArtDetailsBinding?=null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding=FragmentArtDetailsBinding.bind(view)
+        fragmentBinding=binding
+
+        binding.artImageView.setOnClickListener{
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
     }
 
 }
--- app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ArtsFragment.kt
+++ app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/ArtsFragment.kt
@@ -3,11 +3,27 @@ package com.nurgulmantarci.artapphiltkotlin.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nurgulmantarci.artapphiltkotlin.R
+import com.nurgulmantarci.artapphiltkotlin.databinding.FragmentArtsBinding
 
 class ArtsFragment: Fragment(R.layout.fragment_arts) {
 
+   private var fragmentBinding: FragmentArtsBinding?=null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding=FragmentArtsBinding.bind(view)
+        fragmentBinding=binding
+
+        binding.fab.setOnClickListener{
+            findNavController().navigate(ArtsFragmentDirections.actionArtsFragmentToArtDetailsFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        fragmentBinding=null
+        super.onDestroy()
     }
 }
