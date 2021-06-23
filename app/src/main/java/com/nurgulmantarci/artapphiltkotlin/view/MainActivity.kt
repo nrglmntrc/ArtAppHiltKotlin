88c521ad981fa3c6e6770501bc00b9922a5fc55b
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-06-23 16:02:20 +0300

    ui layouts created

--- app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/MainActivity.kt
+++ app/src/main/java/com/nurgulmantarci/artapphiltkotlin/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nurgulmantarci.artapphiltkotlin
+package com.nurgulmantarci.artapphiltkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nurgulmantarci.artapphiltkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
